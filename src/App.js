35ef4faed1fdd57502848a16a83fbb3da0576361
Chester <cheung319@gmail.com>
2022-04-10 21:35:28 -0400

    Created Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
-      <h1><Home/></h1>
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
