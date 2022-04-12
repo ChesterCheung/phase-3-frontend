ced4e977a36bd3870bf96ba18e82ac04c062fccc
Chester Cheung <cheung319@gmail.com>
2022-04-11 22:20:08 -0400

    App update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import WorkoutContainer from "./components/WorkoutContainer";
+import WorkoutForm from "./components/WorkoutForm";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-    </div>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/workouts/new" element={<WorkoutForm/>}></Route>
+            <Route path="/workouts" element={<WorkoutContainer/>}></Route>
+          </Routes>
+    </Router>
   );
 }
 
