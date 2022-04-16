55424f511a651df31cf3b7e9868e4078bc2a85ad
Chester <cheung319@gmail.com>
2022-04-16 18:30:17 -0400

    completed

--- src/components/WorkoutContainer.jsx
+++ src/components/WorkoutContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import WorkoutCard from './WorkoutCard'
 
 const WorkoutContainer = ({workout, handleDelete}) => {
-  const workoutCards= workout.map(workout => <h1><WorkoutCard handleDelete={handleDelete} key={workout.id} workout={workout}/></h1>)
+  const workoutCards= workout.map(workout => <h1 key={workout.id}><WorkoutCard handleDelete={handleDelete} workout={workout}/></h1>)
   return (
     <div>
     {workoutCards}
