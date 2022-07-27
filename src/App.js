231db4e9b40e64ebd2521570d5c0402c0da203a7
Chester Cheung <cheung319@gmail.com>
2022-07-27 11:38:54 -0400

    changed globals, added css font

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import WorkoutContainer from "./components/WorkoutContainer";
-import WorkoutForm from "./components/WorkoutForm";
+import ExerciseContainer from "./components/ExerciseContainer";
+import ExerciseForm from "./components/ExerciseForm";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { useState, useEffect } from "react";
 import { baseURL } from "./Globals";
+import WorkoutList from "./components/WorkoutList";
 
 
 const App = () => {
+  const [exercises, setExercises] = useState([])
   const [workouts, setWorkouts] = useState([])
 
   useEffect(()=> {
-    fetch(baseURL + "/workouts")
+    fetch(baseURL + "/exercises")
+      .then(resp => resp.json())
+      .then(data => setExercises(data))
+
+      fetch(baseURL + "/workouts")
       .then(resp => resp.json())
       .then(data => setWorkouts(data))
+
   },[])
 
-  const addWorkout = (workout) => {
-    setWorkouts([...workouts, workout])
+  const addExercise = (exercise) => {
+    setExercises([...exercises, exercise])
   }
 
+  const workoutDeleteExercise = exercise => {
+    setWorkouts({
+      ...workouts,
+      exercises: workouts.exercises.filter(e => e.id !== exercise.id)
+    })
+  }
+  
+  
   const handleDelete = (workoutObj) =>{
-    fetch(`http://localhost:4000/workouts/${workoutObj.id}`,{
+    fetch(`http://localhost:9292/exercises/${workoutObj.id}`,{
       method: "DELETE"
     })
       .then(()=>{
-        const filteredWorkouts = workouts.filter(exercise => exercise.id !== workoutObj.id)
-        setWorkouts(filteredWorkouts)
+        const filteredWorkouts = exercises.filter(exercise => exercise.id !== workoutObj.id)
+        setExercises(filteredWorkouts)
       })
   }
 
@@ -36,8 +51,9 @@ const App = () => {
         <Navbar/>
           <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/workouts/new" element={<WorkoutForm addWorkout={addWorkout} setWorkouts={setWorkouts} baseURL={baseURL}/>}></Route>
-            <Route path="/workouts" element={<WorkoutContainer handleDelete={handleDelete} workouts={workouts}/>}></Route>
+            <Route path="/exercises/new" element={<ExerciseForm addExercise={addExercise} setExercises={setExercises} baseURL={baseURL} workouts={workouts}/>}></Route>
+            <Route path="/exercises" element={<ExerciseContainer workoutDeleteExercise={workoutDeleteExercise} handleDelete={handleDelete} exercises={exercises}/>}></Route>
+            <Route path="/workouts" element={<WorkoutList workouts={workouts} />}> </Route>
           </Routes>
       </Router>
   );
