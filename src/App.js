6586a1e256ed96f3c79d8383291d3ed3fce6e10c
Chester Cheung <cheung319@gmail.com>
2022-07-31 15:02:06 -0400

    added function for edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import ExerciseContainer from "./components/ExerciseContainer";
+import ExerciseList from "./components/ExerciseList";
 import ExerciseForm from "./components/ExerciseForm";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -17,42 +17,67 @@ const App = () => {
     fetch(baseURL + "/exercises")
       .then(resp => resp.json())
       .then(data => setExercises(data))
+  },[])
 
-      fetch(baseURL + "/workouts")
+  useEffect(()=> {
+    fetch(baseURL + "/workouts")
       .then(resp => resp.json())
       .then(data => setWorkouts(data))
-
   },[])
 
   const addExercise = (exercise) => {
     setExercises([...exercises, exercise])
   }
 
-  const workoutDeleteExercise = exercise => {
-    setWorkouts({
-      ...workouts,
-      exercises: workouts.exercises.filter(e => e.id !== exercise.id)
-    })
+  const addWorkout = (workout) => {
+    console.log(workout)
+    setWorkouts([...workouts, workout])
+  }
+
+  function handleUpdateExercise (updatedExercise) {
+    const updated = exercises.map((exercise) => {
+      if (exercise.id === updatedExercise.id) {
+        return updatedExercise;
+      } else {
+        return exercise;
+      }
+    });
+    setExercises(updated);
   }
   
-  
-  const handleDelete = (workoutObj) =>{
-    fetch(`http://localhost:9292/exercises/${workoutObj.id}`,{
-      method: "DELETE"
+  const deleteExercise = (exercise) => {
+    setExercises(exercises.filter(e => e.id !== exercise.id)
+  )}
+
+  const handleDelete = (e) =>{
+    fetch(`http://localhost:9292/exercises/${e.id}`,{
+      method: "DELETE",
     })
-      .then(()=>{
-        const filteredWorkouts = exercises.filter(exercise => exercise.id !== workoutObj.id)
-        setExercises(filteredWorkouts)
+      .then (resp => resp.json())
+      .then((data) => {
+        deleteExercise(data)
+        if(workouts) {
+        authorDeleteBlog(data)
+        }
       })
   }
+
+  const authorDeleteBlog = exercise => {
+    setWorkouts(
+      ...workouts,
+      [workouts.exercises.filter(e => e.id !== exercise.id)]
+    )
+  }
+
+  
 
   return (
       <Router>
         <Navbar/>
           <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/exercises/new" element={<ExerciseForm addExercise={addExercise} setExercises={setExercises} baseURL={baseURL} workouts={workouts}/>}></Route>
-            <Route path="/exercises" element={<ExerciseContainer workoutDeleteExercise={workoutDeleteExercise} handleDelete={handleDelete} exercises={exercises}/>}></Route>
+            <Route path="/exercises/new" element={<ExerciseForm setWorkouts={setWorkouts} addWorkout={addWorkout} addExercise={addExercise} setExercises={setExercises} baseURL={baseURL} workouts={workouts}/>}></Route>
+            <Route path="/exercises" element={<ExerciseList handleUpdateExercise={handleUpdateExercise} handleDelete={handleDelete} exercises={exercises}/>}></Route>
             <Route path="/workouts" element={<WorkoutList workouts={workouts} />}> </Route>
           </Routes>
       </Router>
